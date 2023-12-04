e85faaf17eb355a82c18d30d33cc51ac074eb142
Leo Euclides <leoeuclidss@gmail.com>
2023-12-04 17:52:01 -0300

    Fix property model inverse relationship

--- app/mirage/models/property.js
+++ app/mirage/models/property.js
@@ -2,6 +2,6 @@ import { Model, belongsTo, hasMany } from 'miragejs';
 
 export default Model.extend({
   entity: belongsTo('entity', { inverse: 'properties' }),
-  parent: belongsTo('property', { inverse: 'children' }),
+  parent: belongsTo('property', { inverse: 'properties' }),
   properties: hasMany('property', { inverse: 'parent' }),
 });
